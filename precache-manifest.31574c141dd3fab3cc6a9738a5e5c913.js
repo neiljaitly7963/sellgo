463eb2bb880fa6e978982ab995add4496301fc7b
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-15 22:58:46 +0530

    Updates

---
+++ precache-manifest.31574c141dd3fab3cc6a9738a5e5c913.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2ff4237a983215ac1ae40600e6b8a9",
+    "url": "/sellgo/index.html"
+  },
+  {
+    "revision": "b22a7dc804d95df0290a",
+    "url": "/sellgo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c694c8bb102584618df4",
+    "url": "/sellgo/static/css/main.cd6ceb76.chunk.css"
+  },
+  {
+    "revision": "b22a7dc804d95df0290a",
+    "url": "/sellgo/static/js/2.09a72045.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sellgo/static/js/2.09a72045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c694c8bb102584618df4",
+    "url": "/sellgo/static/js/main.2faf0293.chunk.js"
+  },
+  {
+    "revision": "a681451cfb260b640103",
+    "url": "/sellgo/static/js/runtime-main.5fe708cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sellgo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f59b02762cdab1c3e6beb1acb53b7ebc",
+    "url": "/sellgo/static/media/search.f59b0276.svg"
+  }
+]);
